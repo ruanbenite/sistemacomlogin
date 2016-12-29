3a298d4ba60abe0cde14ddb971ab396ae3bb9f83
Ruan <Ruan@Ruan>
2016-12-29 18:00:12 -0200

    ajuste

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$dns = "mysql:dbname=sistemacomlogin;host=localhost";
+$dbuser = "root";
+$dbpassword = "";
+try {
+
+    $pdo = new PDO($dns, $dbuser, $dbpassword);
+} catch (PDOException $e) {
+
+    echo"Falha ao conectar no banco" . $e->getMessage();
+}
--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+require 'config.php';
+?>
+<form method="POST">
+    <table border="15px" width="100%">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Senha</th>
+            <th>Status</th>
+        </tr>
+        <?php
+        $sql = "SELECT * FROM usuarios";
+        $sql = $pdo->query($sql);
+        if ($sql->rowCount() > 0) {
+            foreach ($sql->fetchAll() as $dados) {
+                echo"<tr>";
+                echo"<td>" . $dados['usu_nome'] . "</td>";
+                echo"<td>" . $dados['usu_email'] . "</td>";
+                echo"<td>" . $dados['usu_senha'] . "</td>";
+                echo"<td>" . $dados['usu_ativo'] . "</td>";
+                echo"</tr>";
+            }
+        }
+        ?>
+    </table>
+</form>
